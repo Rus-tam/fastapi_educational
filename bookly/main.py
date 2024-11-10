8e7c2f48a5c3c5a5f1c31297b56ad95ba919c340
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2024-11-10 21:14:32 +0500

    book_data

--- bookly/main.py
+++ bookly/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get('/')
-async def read_root():
+async def read_root() -> dict:
     return {"message": "Hello world"}
 
-@app.get('/greet/{name}')
-async def greet_name(name: str) -> dict:
-    return {"message": f"Hello {name}"}
+@app.get('/greet/{name}/{age}')
+async def greet_name(name: str, age: int) -> dict:
+    return {"message": f"Hello, {name}. Age {age}"}
+
+
+class BookCreateModel(BaseModel):
+    title: str
+    author: str
+
+@app.post('/create_book')
+async def create_book(book_data: BookCreateModel):
+    return {
+        "title": book_data.title,
+        "author": book_data.author
+    }
