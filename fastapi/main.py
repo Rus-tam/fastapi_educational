3f498d40d6a4cdd3626dac58af8624d4c4bb23e7
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2024-11-18 00:18:02 +0500

    some changes

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+
+@app.post("/create")
+async def creat_posts(post: Post):
+    return {"new_post": f"title: {post.title} content: {post.content}"}
