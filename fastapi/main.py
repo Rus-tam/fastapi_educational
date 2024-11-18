10ed9ba6b0610c92a5fc9c87876aff06142e8164
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2024-11-18 17:03:41 +0500

    some crud

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -12,11 +13,30 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+my_posts = [
+    {"title": "title of post 1", "content": "content of post 1", "id": 1},
+    {"title": "title of post 2", "content": "content of post 2", "id": 2},
+]
+
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World!"}
+    return {"data": my_posts}
 
 
-@app.post("/create")
+@app.post("/posts")
 async def creat_posts(post: Post):
-    return {"new_post": f"title: {post.title} content: {post.content}"}
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 100000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    res_post: Post = {}
+    for post in my_posts:
+        if post["id"] == id:
+            res_post = post
+
+    return res_post
