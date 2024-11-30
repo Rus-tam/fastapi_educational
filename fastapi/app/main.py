8f1af12548e7f12defa3f54a194aa2dbb35e41cd
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2024-11-30 21:56:40 +0500

    connection to db with sqlalchemy

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status, Response
+from fastapi import FastAPI, status, Response, Depends
 from pydantic import BaseModel
 from typing import List
 from fastapi.exceptions import HTTPException
@@ -6,102 +6,17 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from datetime import datetime
+from sqlalchemy.orm import Session
+from . import models
+from .database import engine, get_db
+
+
+models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
 
 
-class Post(BaseModel):
-    id: int
-    title: str
-    content: str
-    published: bool = True
-    created_at: datetime
-
-
-while True:
-    try:
-        conn = psycopg2.connect(
-            host="localhost",
-            database="fastapi_db",
-            user="postgres",
-            password="root",
-            cursor_factory=RealDictCursor,
-        )
-        cursor = conn.cursor()
-        print("Database connection was succesfull!")
-        break
-    except Exception as error:
-        print("Connecting to db failed")
-        print("Error: ", error)
-        time.sleep(2)
-
-
-my_posts: List[Post] = [
-    {"title": "title of post 1", "content": "content of post 1", "id": 1},
-    {"title": "title of post 2", "content": "content of post 2", "id": 2},
-]
-
-
-@app.get("/", status_code=status.HTTP_200_OK, response_model=List[Post])
-async def get_posts():
-    cursor.execute("""SELECT * FROM posts;""")
-    posts = cursor.fetchall()
-    return posts
-
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=Post)
-async def create_posts(post: Post):
-    cursor.execute(
-        """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *;""",
-        (post.title, post.content, post.published),
-    )
-    new_post = cursor.fetchone()
-
-    conn.commit()
-
-    return new_post
-
-
-@app.get("/posts/{id}", status_code=status.HTTP_200_OK, response_model=Post)
-def get_post(id: int):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s;""", (str(id)))
-    post = cursor.fetchone()
-
-    if not post:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
-        )
-    return post
-
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *;""", (str(id),))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-
-    if not deleted_post:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
-        )
-
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.put("/posts/{id}", status_code=status.HTTP_200_OK)
-def update_post(id: int, updates: Post):
-    cursor.execute(
-        """UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *;""",
-        (updates.title, updates.content, updates.published, str(id)),
-    )
-    updated_post = cursor.fetchone()
-    conn.commit()
-
-    if not updated_post:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with id: {id} does not exist",
-        )
-
-    return updated_post
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    return {"status": "success"}
