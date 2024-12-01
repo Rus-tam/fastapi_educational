9800c4be57896917c26be325cd745ae958305535
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2024-12-01 21:55:17 +0500

    get one post

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -33,3 +33,15 @@ def create_post(post: Post, db: Session = Depends(get_db)):
     db.refresh(new_post)
 
     return new_post
+
+
+@app.get("/posts/{id}", status_code=status.HTTP_200_OK, response_model=Post)
+def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
+        )
+
+    return post
